289994b16506de24b30dadb9c0b81e79b16e0bad
Liam W <Liam.Wallace05@Outlook.com>
2025-03-15 13:35:38 +0000

    Unified CSV

--- Assets/Scripts/BeeManager.cs
+++ Assets/Scripts/BeeManager.cs
@@ -1,17 +1,12 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 //This script will be used to load bees from a csv file and manage them
 public class BeeManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+    public class Bee{
         
     }
 }
