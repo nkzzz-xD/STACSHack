9f7049e151820796a6d0068697b10947de2d3985
Matthew Harris <mwillharris@gmail.com>
2025-03-15 23:54:29 +0000

    we need this

---
+++ Assets/FPS/Scripts/Player_Projectile_Movement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerProjectileMovement : MonoBehaviour
+{
+    public float speed = 20f;
+    public float lifetime = 5f;
+    public int damage = 10;
+
+    private void Start()
+    {
+        // Destroy projectile after a certain time
+        // Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
