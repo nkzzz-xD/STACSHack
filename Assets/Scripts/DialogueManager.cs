c9cf700e4e1d498298d0b4a70369afac1eb89bea
LiamK520 <liamkearney520@gmail.com>
2025-03-15 12:06:40 +0000

    please work

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    private bool isTyping;
+
+    private string currentSentence;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue) {
+        sentences.Clear();
+        animator.SetBool("IsOpen", true);
+        isTyping = false;
+
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }
+
+        nameText.text = dialogue.name;
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence() {
+        if (isTyping) {
+            StopAllCoroutines();
+            dialogueText.text = currentSentence;
+            isTyping = false;
+            return;
+        }
+
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        currentSentence = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence) {
+        isTyping = true;
+        dialogueText.text = "";
+
+        foreach (char letter in sentence.ToCharArray()) {
+            dialogueText.text += letter;
+            // one char per frame
+            yield return new WaitForSeconds(0.025f);
+        }
+
+        isTyping = false;
+    }
+
+    void EndDialogue() {
+        Debug.Log("End of conversation");
+        animator.SetBool("IsOpen", false);
+    }
+}
