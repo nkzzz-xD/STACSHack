38e89cbede453989244646e41c41cc1f18906886
Liam W <Liam.Wallace05@Outlook.com>
2025-03-15 13:23:28 +0000

    Initial scripts and moved files

---
+++ Assets/Scripts/BeeManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+//This script will be used to load bees from a csv file and manage them
+public class BeeManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
