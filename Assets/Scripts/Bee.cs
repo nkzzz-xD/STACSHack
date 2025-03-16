e563af01b348369dac2467123220cb304f7c8524
Liam W <Liam.Wallace05@Outlook.com>
2025-03-16 06:13:34 +0000

    nana sucks

--- Assets/Scripts/Bee.cs
+++ Assets/Scripts/Bee.cs
@@ -122,7 +122,7 @@ public class Bee : MonoBehaviour
 
         Destroy(gameObject); // Remove the bee
 
-        if (badBeesFound == NewBeeSpawner.badBeeCount)
+        if (badBeesFound == 4)
         {
             Debug.Log("All bad bees deleted. Loading new scene...");
             SceneManager.LoadScene("ExecutionScene"); // Change "NextScene" to your actual scene name
